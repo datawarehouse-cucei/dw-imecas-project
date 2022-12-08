6023d0f93dec1fc6b38321c26d161f571c1c6f4a
Oscar Quintero <oscar24quintero@hotmail.com>
2022-12-07 23:38:27 -0600

    Servicio de limpieza de postgres y mysql

--- fastapi-python/mainFAPI.py
+++ fastapi-python/mainFAPI.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import petl as etl
 import time
 
 app = FastAPI()
@@ -28,12 +29,31 @@ def test_dw():
 
 @app.get('/mysql-db-cleaning/')
 def mysql_db_cleaning():
-    return {'message': 'Limpieza de la Base de Datos MySQL Lista'}
+    table1 = etl.fromcsv("./recursos/2018pm10.csv")
+    etl.tojson(table1, './results-json/mysqlclean2018pm10.json', sort_keys=False)
+    jsonRes = open("./results-json/mysqlclean2018pm10.json").read()
+    return [{'message': 'Limpieza de la Base de Datos MySQL Lista'}, jsonRes]
 
 @app.get('/postgresql-db-cleaning/')
 def postgresql_db_cleaning():
-    return {'message': 'Limpieza de la Base de Datos Postgres Lista'}
+    table1 = etl.fromcsv("./recursos/acc2018.csv")
+    etl.tojson(table1, './results-json/postgrescleanacc2018.json', sort_keys=False)
+    jsonRes = open("./results-json/postgresacc2018.json").read()
+    return [{'message': 'Limpieza de la Base de Datos Postgres Lista'}, jsonRes]
+    
 
 @app.get('/process')
 def process():
     return {'message': 'Datos procesados'}
+
+@app.get('/autos')
+def consultar_autos():
+    return {'message': 'Autos'}
+
+@app.get('/zonas')
+def consultar_zonas():
+    return {'message': 'Zonas'}
+
+@app.get('/calidad-del-aire')
+def consultar_calidad_del_aire():
+    return {'message': 'Calidad del aire'}
